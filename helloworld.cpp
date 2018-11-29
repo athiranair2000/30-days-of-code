236bc90d0125a84ee509fb0975fbb0c55280cc5c
Athi_Nair_2000@github <43814992+athiranair2000@users.noreply.github.com>
2018-11-29 21:32:26 +0530

    "Hello World!"

---
+++ helloworld.cpp
@@ -0,0 +1,15 @@
+int main() {
+    // Declare a variable named 'input_string' to hold our input.
+    char input_string[50]; 
+    
+    // Read a full line of input from stdin (cin) and save it to our variable, input_string.
+    gets(input_string);
+    
+    // Print a string literal saying "Hello, World." to stdout using cout.
+    cout << "Hello, World." << endl;
+
+    // TODO: Write a line of code here that prints the contents of input_string to stdout.
+    cout<<input_string<<endl;
+
+    return 0;
+}
