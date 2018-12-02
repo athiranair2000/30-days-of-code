426b98d032f88e96a4bed9b7095bab05a615ad7b
Athi_Nair_2000@github <43814992+athiranair2000@users.noreply.github.com>
2018-12-02 21:47:47 +0530

    Day 1
    "Solutions to Data operators"

---
+++ day1.cpp
@@ -0,0 +1,22 @@
+int t;
+double u;
+string su;
+int sum;
+double  sum1;
+string s2;
+cin>>t>>u;
+getline(cin>>ws,su);
+sum=i+t;
+sum1=u+d;
+s2=s+su;
+cout<<sum<<endl<<fixed<<setprecision(1)<<sum1<<endl<<s2;
+    // Declare second integer, double, and String variables.
+    
+    // Read and save an integer, double, and String to your variables.
+    
+    // Print the sum of both integer variables on a new line.
+    
+    // Print the sum of the double variables on a new line.
+    
+    // Concatenate and print the String variables on a new line
+    // The 's' variable above should be printed first.
