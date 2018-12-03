384e0e4a8c84273a2ebaeca39f9e33f89aac11e6
Athi_Nair_2000@github <43814992+athiranair2000@users.noreply.github.com>
2018-12-04 00:42:25 +0530

    loops
    "solutions to loops"

---
+++ Day5.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+
+int main()
+{
+    int x=1,i=1,m;
+    int n;
+    cin>>n;
+    while(i<=10)
+    {
+
+        m=n*i;
+    cout<<n<<" "<<"x"<<" "<<i<<" "<< "=" <<" "<< m<<endl;
+        i++;
+         x++;
+    }
+
+}
+
