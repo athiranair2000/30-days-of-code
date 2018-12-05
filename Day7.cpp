800805ce47667e03f22defe03ce362743d3e996c
Athi_Nair_2000@github <43814992+athiranair2000@users.noreply.github.com>
2018-12-05 14:28:10 +0530

    Arrays
    "solutions to Day7-Arrays"

---
+++ Day7.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+vector<string> split_string(string);
+
+
+
+int main ()
+{
+    int n,i;
+    
+    cin>>n;
+    
+    int arr[n];
+    for (i=0;i<n;i++) {
+        arr[i];
+     cin>>arr[i];   
+    }
+    reverse(arr,arr+n);
+    
+      for (i=0;i<n;i++) {
+          arr[i];
+      cout<<arr[i]<<" ";
+      }
+ return 0;
+}
+
+
+    
